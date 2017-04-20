3c7b878995a0c5bcda9394c271528ff87859f32c
Randy Secrist <randy.secrist@gmail.com>
2017-04-20 15:45:27 -0600

    response 204

--- apps/api/lib/api/router.ex
+++ apps/api/lib/api/router.ex
@@ -6,19 +6,21 @@ defmodule API.Router do
   plug :dispatch
 
   post "/v1/event/:realm/:domain/:entity_id/:event_type/:event_id" do
-    send_resp(conn, 200, Poison.encode!(%{
-      realm: realm,  # "nike"
-      domain: domain,  # "challenge"
-      entity_id: entity_id,  # group by these
-      event_type: event_type,
-      event_id: event_id
-    }))
+    send_resp(conn, 204, "")
+    # send_resp(conn, 200, Poison.encode!(%{
+    #   realm: realm,  # "nike"
+    #   domain: domain,  # "challenge"
+    #   entity_id: entity_id,  # group by these
+    #   event_type: event_type,  # FSM?  start, stop, edit, delete, actual goal
+    #   event_id: event_id
+    # }))
   end
 
   post "/v1/events/:realm" do
-    send_resp(conn, 200, Poison.encode!(%{
-      count: 0
-    }))
+    send_resp(conn, 204, "")
+    # send_resp(conn, 200, Poison.encode!(%{
+    #   count: 0
+    # }))
   end
 
   match _ do
--- apps/api/test/api_test.exs
+++ apps/api/test/api_test.exs
@@ -6,13 +6,13 @@ defmodule APITest do
 
   @api_router_opts API.Router.init([])
 
-  test "create event" do
-    realm = "realm"
-    domain = "domain"
-    entity_id = "entity_id"
-    event_type = "event_type"
-    event_id = "event_id"
-    fixture = "Some DATA"
+  test "create challenge event" do
+    realm = "company"
+    domain = "challenge"
+    entity_id = "uuid_v4"
+    event_type = "state"
+    event_id = "uuid_v1"
+    fixture = "Some|Data|Structure,Random|But|Limited|Length"
     conn = conn(:post, "/v1/event/#{realm}/#{domain}/#{entity_id}/#{event_type}/#{event_id}", fixture)
     |> put_req_header("content-type", "application/json")
     conn = API.Router.call(conn, @api_router_opts)
