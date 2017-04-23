3edb286af2057fedf4bed899e35b460906448d1b
Randy Secrist <randy.secrist@gmail.com>
2017-04-23 12:31:21 -0600

    push api logs to a file

--- config/config.exs
+++ config/config.exs
@@ -6,21 +6,17 @@ import_config "*local.exs"
 config :ssl, protocol_version: :"tlsv1.2"
 
 config :logger,
-        backends: [:console],
-        utc_log: true,
-        level: :warn,
-        truncate: 4096
+  backends: [
+    {FileLoggerBackend, :error_log},
+    {FileLoggerBackend, :access_log}],
+  utc_log: true,
+  compile_time_purge_level: :debug,
+  truncate: 4096
 
-# config :logger,
-#   backends: [{LoggerFileBackend, :error_log}]
-#   # utc_log: true,
-#   # compile_time_purge_level: :info,
-#   # truncate: 4096
-#
-# config :logger, :error_log,
-#   path: "log/error.log",
-#   level: :error
+config :logger, :access_log,
+  path: System.cwd <> "/log/access.log",
+  level: :info
 
-# config :quantum,
-#   global?: true,
-#   timezone: "America/Denver"
+config :logger, :error_log,
+  path: System.cwd <> "/log/error.log",
+  level: :error
