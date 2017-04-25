fc8487b4113d197092c7cc197ff0db97d62ecabf
Randy Secrist <randy.secrist@gmail.com>
2017-04-25 14:27:24 -0600

    push events to backend

--- apps/api/lib/api/event.ex
+++ apps/api/lib/api/event.ex
@@ -1,13 +1,17 @@
 defmodule API.Event do
   import Plug.Conn
 
-  def post(conn) do
-    json = conn.body_params
-    send_resp(conn, 204, "")
+  @spec post(Plug.Conn, DB.Event.t) :: Plug.Conn
+  def post(conn, %DB.Event{} = event) do
+    case DB.Event.save(%{event | meta: conn.body_params}) do
+      %DB.Event{}=event ->
+        send_resp(conn, 204, "")
+      _ -> send_resp(conn, 500, "General Error")
+    end
   end
 
   def bulk(conn) do
     send_resp(conn, 204, "")
   end
-  
+
 end
