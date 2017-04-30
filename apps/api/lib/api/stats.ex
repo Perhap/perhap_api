85e5d81c07cfbd85d5af0e963213c76cba89ec6f
Randy Secrist <randy.secrist@gmail.com>
2017-04-29 20:33:52 -0600

    only get the value

--- apps/api/lib/api/stats.ex
+++ apps/api/lib/api/stats.ex
@@ -8,7 +8,7 @@ defmodule Stats do
     stats = (c_stat(Exometer.get_value(["event", "GET", :counter], :value), E_Get) |||
              c_stat(Exometer.get_value(["event", "GET", :spiral]), E_Get) |||
              c_stat(Exometer.get_value(["event", "GET", :histogram]), E_Get) |||
-             c_stat(Exometer.get_value(["event", "POST", :counter]), E_Post) |||
+             c_stat(Exometer.get_value(["event", "POST", :counter], :value), E_Post) |||
              c_stat(Exometer.get_value(["event", "POST", :spiral]), E_Post) |||
              c_stat(Exometer.get_value(["event", "POST", :histogram]), E_Post))
 
