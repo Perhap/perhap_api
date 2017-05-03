cf072d45fcc28d123c607bdaf83ce343f9692d52
Randy Secrist <randy.secrist@gmail.com>
2017-05-02 23:01:22 -0600

    fix spacing

--- apps/db/config/prod.exs
+++ apps/db/config/prod.exs
@@ -4,12 +4,12 @@ config :db,
   url: 'perhap-prod-load-1.bigsquid.com',
   unit_separator: "\x1f"
 
-  config :pooler,
-    [pools: [
-      [name: :riakprod1,
-       group: :riak,
-       max_count: 256,
-       init_count: 256,
-       start_mfa: {Riak.Connection, :start_link, ['perhap-lb1.bigsquidapp.com', 8087]}
-      ]
-    ]]
+config :pooler,
+  [pools: [
+    [name: :riakprod1,
+     group: :riak,
+     max_count: 256,
+     init_count: 256,
+     start_mfa: {Riak.Connection, :start_link, ['perhap-lb1.bigsquidapp.com', 8087]}
+    ]
+  ]]
