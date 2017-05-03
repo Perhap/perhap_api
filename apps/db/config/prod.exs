1486fdba5ec43ef41793bcd00f0400b1af998210
Randy Secrist <randy.secrist@gmail.com>
2017-05-02 18:25:32 -0600

    add prod config

--- apps/db/config/prod.exs
+++ apps/db/config/prod.exs
@@ -3,3 +3,13 @@ use Mix.Config
 config :db,
   url: 'perhap-prod-load-1.bigsquid.com',
   unit_separator: "\x1f"
+
+  config :pooler,
+    [pools: [
+      [name: :riakprod1,
+       group: :riak,
+       max_count: 256,
+       init_count: 256,
+       start_mfa: {Riak.Connection, :start_link, ['perhap-lb1.bigsquidapp.com', 8087]}
+      ]
+    ]]
