5ebb80c5fbf32e288d417208f33ffee8a716f2a8
Randy Secrist <randy.secrist@gmail.com>
2017-05-08 13:14:14 -0600

    add async task supervisor

--- apps/api/lib/api.ex
+++ apps/api/lib/api.ex
@@ -5,25 +5,30 @@ defmodule API do
     import Supervisor.Spec, warn: false
 
     children = [
-      case Application.get_env(:api, :use_ssl) do
-        false ->
-          Plug.Adapters.Cowboy.child_spec(:http, API.Router, [], [port: Application.get_env(:api, :port)])
-        true ->
-          ssl_options = Application.get_env(:api, :ssl_options)
-          cowboy_options = [
-            port: Application.get_env(:api, :port),
-            otp_app: :api,
-            keyfile: ssl_options[:keyfile],
-            certfile: ssl_options[:certfile],
-          ]
-          cowboy_options = case Keyword.has_key?(ssl_options, :cacertfile) do
-            true -> Keyword.put_new(cowboy_options, :cacertfile, ssl_options[:cacertfile])
-            false -> cowboy_options
-          end
-          Plug.Adapters.Cowboy.child_spec(:https, API.Router, [], cowboy_options)
-      end
+      supervisor(Task.Supervisor, [[name: Perhap.TaskSupervisor]]),
+      cowboy_child_spec()
     ]
     opts = [strategy: :one_for_one, name: API.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp cowboy_child_spec() do
+    case Application.get_env(:api, :use_ssl) do
+      false ->
+        Plug.Adapters.Cowboy.child_spec(:http, API.Router, [], [port: Application.get_env(:api, :port)])
+      true ->
+        ssl_options = Application.get_env(:api, :ssl_options)
+        cowboy_options = [
+          port: Application.get_env(:api, :port),
+          otp_app: :api,
+          keyfile: ssl_options[:keyfile],
+          certfile: ssl_options[:certfile],
+        ]
+        cowboy_options = case Keyword.has_key?(ssl_options, :cacertfile) do
+          true -> Keyword.put_new(cowboy_options, :cacertfile, ssl_options[:cacertfile])
+          false -> cowboy_options
+        end
+        Plug.Adapters.Cowboy.child_spec(:https, API.Router, [], cowboy_options)
+    end
   end
 end
