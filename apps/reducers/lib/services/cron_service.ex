20dea7743686cab1944253c62667553614d36c75
Selena Rakowski <selenarakowski@gmail.com>
2017-06-01 10:11:33 -0600

    fixed cron event

--- apps/reducers/lib/services/cron_service.ex
+++ apps/reducers/lib/services/cron_service.ex
@@ -16,9 +16,10 @@ defmodule Service.Cron do
     event_id = gen_uuidv1()
     {_, data} =Poison.encode(eventdata)
 
-    url = "https://perhap.bigsquidapp.com/v1/event/domo/" <> entity_id <> "/pull/" <> event_id
+    url = "https://perhap.bigsquidapp.com/v1/event/nike/domo/" <> entity_id <> "/pull/" <> event_id
     Logger.info("bin audit request from domo", perhap_only: 1)
-    HTTPoison.post(url, data, [], [])
+    response = HTTPoison.post(url, data, ["Content-Type": "application/json"], [])
+    response
   end
 
   def actuals_event() do
@@ -34,9 +35,10 @@ defmodule Service.Cron do
     event_id = gen_uuidv1()
     {_, data} =Poison.encode(eventdata)
 
-    url = "https://perhap.bigsquidapp.com/v1/event/domo/" <> entity_id <> "/pull/" <> event_id
+    url = "https://perhap.bigsquidapp.com/v1/event/nike/domo/" <> entity_id <> "/pull/" <> event_id
     Logger.info("actuals request from domo", perhap_only: 1)
-    HTTPoison.post(url, data, [], [])
+    response = HTTPoison.post(url, data, ["Content-Type": "application/json"], [])
+    response
   end
 
   def heartbeat() do
