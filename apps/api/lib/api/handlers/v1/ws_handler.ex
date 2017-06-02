a65ff795112b64fd749db00c582baf2e44be2b69
Randy Secrist <randy@MacBook-Pro.domain>
2017-06-02 10:45:12 -0600

    simple websocket implementation

--- apps/api/lib/api/handlers/v1/ws_handler.ex
+++ apps/api/lib/api/handlers/v1/ws_handler.ex
@@ -1,6 +1,26 @@
 defmodule API.WSHandler do
   use API.Handler
   def init(req0, opts) do
-    {:ok, req0, opts}
+    {:cowboy_websocket, req0, opts}
+  end
+
+  def websocket_init(state) do
+    :erlang.start_timer(1000, self(), "Hello")
+    {:ok, state}
+  end
+
+  def websocket_handle({:text, message}, state) do
+    {:reply, {:text, "That's what she said! - #{message}"}, state}
+  end
+  def websocket_handle(_data, state) do
+    {:ok, state}
+  end
+
+  def websocket_info({:timeout, _ref, message}, state) do
+    :erlang.start_timer(1000, self(), "How are you?")
+    {:reply, {:text, message}, state}
+  end
+  def websocket_info(_info, state) do
+    {:ok, state}
   end
 end
